e1cd89e8255872ba83bcb0d1402773f9e397a88a
damonmoya <damon99@hotmail.es>
2022-10-05 15:45:06 +0100

    Redux basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { seSelector, useSelector } from 'react-redux';
+import TaskForm from './components/TaskForm';
+import TasksList from './components/TasksList';
 
 function App() {
+
+  const tasksState = useSelector(state => state.tasks);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>        
+      <h1>Hello World</h1>
+
+      <TaskForm/>
+      <TasksList/>  
     </div>
   );
 }
